29d56ac2e264d493dfa8e917d396453616653284
Cecile Dai <ceciled126@gmail.com>
2022-11-04 21:27:32 -0400

    api finally running but configservice isn't working

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './modules/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = this.configService.get('NEST_APP_PORT');
-  
-  await app.listen(port);
+  // const port = this.configService.get('NEST_APP_PORT');
+
+  // await app.listen(port);
+  await app.listen(9999);
 }
 bootstrap();
--- api/src/modules/app.module.ts
+++ api/src/modules/app.module.ts
@@ -1,31 +1,49 @@
 import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
-import { AppController } from '../app.controller';
-import { AppService } from '../app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoggerMiddleware } from 'src/common/logger.middleware';
 import { GHCNMController } from './ghcnmv2/ghcnm.controller';
 import { GHCNModule } from './ghcnmv2/ghcnm.module';
 
-// @Module({
-//   imports: [ConfigModule.forRoot({
-//     envFilePath: ['.env.dev'],
-//     isGlobal: true
-//   })],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      
-    }),
     ConfigModule.forRoot({
       envFilePath: `${process.cwd()}/.env.${process.env.NODE_ENV}`,
       isGlobal: true
-    })
+    }),
+    TypeOrmModule.forRoot({
+      // type: 'postgres',
+      // host: process.env.DATABASE_HOST,
+      // port: parseInt(process.env.DATABASE_PORT ?? '5432'),
+      // username: process.env.DATABASE_USER,
+      // password: process.env.DATABASE_PASSWORD,
+      // database: process.env.DATABASE_NAME,
+      // entities: [__dirname + '/**/*.entity.ts'],
+      // synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE)
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'data126',
+      database: 'climatedata',
+      entities: [__dirname + '/**/*.entity.ts'],
+      synchronize: true
+    }),
+    // TypeOrmModule.forRootAsync({
+    //   imports: [ConfigModule],      
+    //   useFactory: (configService: ConfigService) => ({
+    //     type: 'postgres' as 'postgres',
+    //     host: configService.get<string>('DATABASE_HOST'),
+    //     port: parseInt(configService.get<string>('DATABASE_PORT') ?? '5432'),
+    //     username: configService.get<string>('DATABASE_USER'),
+    //     password: configService.get<string>('DATABASE_PASSWORD'),
+    //     database: configService.get<string>('DATABASE_NAME'),
+    //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //     synchronize: true,
+    //   }),
+    //   inject: [ConfigService],
+    // }),
+    GHCNModule,
   ]
 })
 
