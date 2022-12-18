7065a8587471dbbb6427b61547853aece930780f
Cecile Dai <ceciled126@gmail.com>
2022-12-18 10:30:50 -0800

    modifications to try to get it running on the right server

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
 
-  await app.listen(parseInt(process.env.NEST_APP_PORT ?? '9999'), process.env.NEST_APP_HOST ?? '0.0.0.0');
+  await app.listen(parseInt(process.env.NEST_APP_PORT ?? '9999'), '0.0.0.0', 
+    async function() { console.log(`Listening on ${await app.getUrl()}`) });
 }
 bootstrap();
--- frontend/src/services/GHCNMService.ts
+++ frontend/src/services/GHCNMService.ts
@@ -3,31 +3,31 @@ import { getHeaders } from '../common/constants';
 import axios from 'axios';
 
 export async function getAllStationMetadata(): Promise<StationMetadata[]> {
-  return axios.get('http://localhost:9999/api/ghcnmv2/station', {
+  return axios.get('http://0.0.0.0:9999/api/ghcnmv2/station', {
     headers: getHeaders
   }).then(res => res.data);
 }
 
 export async function getAllBasicStationMetadata(): Promise<StationMetadataBasic[]> {
-  return axios.get('http://localhost:9999/api/ghcnmv2/basic/station', {
+  return axios.get('http://0.0.0.0:9999/api/ghcnmv2/basic/station', {
     headers: getHeaders
   }).then(res => res.data);
 }
 
 export async function getAllCountries(): Promise<CountryInfo[]> {
-  return axios.get('http://localhost:9999/api/ghcnmv2/countries', {
+  return axios.get('http://0.0.0.0:9999/api/ghcnmv2/countries', {
     headers: getHeaders
   }).then(res => res.data);
 }
 
 export async function getAllRegions(): Promise<RawRegions[]> {
-  return axios.get('http://localhost:9999/api/ghcnmv2/regions', {
+  return axios.get('http://0.0.0.0:9999/api/ghcnmv2/regions', {
     headers: getHeaders
   }).then(res => res.data);
 }
 
 export async function getDownloadData(byStation: boolean, dataParams: ParamsFields): Promise<any> {
-  return axios.get('http://localhost:9999/api/ghcnmv2/download', {
+  return axios.get('http://0.0.0.0:9999/api/ghcnmv2/download', {
     headers: getHeaders,
     params: {
       byStation,
