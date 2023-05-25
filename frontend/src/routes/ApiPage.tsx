e077f823dc704649988387a300a47ea2cfd32246
Cecile Dai <ceciled126@gmail.com>
2023-05-25 11:33:58 -0400

    started API page

--- frontend/src/routes/ApiPage.tsx
+++ frontend/src/routes/ApiPage.tsx
@@ -1,16 +1,44 @@
+import { useState } from "react";
+
 export default function ApiPage() {
+
+  const [downloadFormat, setDownloadFormat] = useState<'csv' | 'json'>('csv');
+
   return (
-    <div className="data-content"
-      style={{ height: '100vh' }} >
+    <div className="data-content" style={{ height: '100vh' }} >
       <div className='title'>
         Historical Precipitation Data Explorer
       </div>
       <hr style={{ width: "100%" }} />
-      <div className="params-section h-25">
-        <div className='heading-1'>
-          In development!
+      <div className="d-flex flex-row" style={{ height: "85%" }}>
+        <div className="plain-section w-100" style={{ overflow: 'auto' }}>
+          <div className='heading-1'>
+            API
+          </div>
+          <div className='heading-2'>
+            Parameters
+          </div>
+        </div>
+        <div className="d-flex flex-column w-100 ms-3">
+          <div className="text-area">
+
+          </div>
+          <div className="d-flex flex-row mx-auto">
+            <div className="mt-2">Download as</div>
+            <select className="mt-2 ms-2" value={downloadFormat} onChange={(e: any) => setDownloadFormat(e.target.value)}>
+              <option value='csv'>CSV</option>
+              <option value='json'>JSON</option>
+            </select>
+          </div>
+          <button className='med-button mx-auto'>
+            <div className='med-button-text'>
+              DOWNLOAD
+            </div>
+            <i className='material-icons'>play_arrow</i>
+          </button>
         </div>
       </div>
+
     </div>
   )
 }
